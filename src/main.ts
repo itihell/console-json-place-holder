617c0be8c3a49fd248a19311bc5c294332cb734a
Itihell Mejia <itihell.mejia@gmail.com>
2024-10-23 18:52:48 -0600

    Agregando archivo de barril para utils

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { getPost } from "./controllers/post";
 import { getUsers } from "./controllers/users";
 import { Post } from "./interface/posts.interface";
 import { User } from "./interface/users.interface";
-import { getUserIdByPost } from "./utils/getUserIdByPost";
-import { mergePostWithUsers } from "./utils/mergePostWithUsers";
+import { setIndexUser,mergePostWithUsers } from "./utils";
+
 
 const viewData = async () => {
   const posts: Post[] = await getPost();
   const users: User[] = await getUsers();
 
   const customPosts = mergePostWithUsers(posts, users);
+  const userIndexed = setIndexUser(users)
 
   console.log(users);
 };
