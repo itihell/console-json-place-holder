98900cfff18be9a1b44f7a3ad352521da9b245b8
Itihell Mejia <itihell.mejia@gmail.com>
2024-11-07 19:04:18 -0600

    Ajuste de la interface de usuarios

--- src/interface/users.interface.ts
+++ src/interface/users.interface.ts
@@ -2,11 +2,11 @@ export interface User {
   id: number;
   name: string;
   username: string;
-  email: string;
-  address: Address;
-  phone: string;
-  website: string;
-  company: Company;
+  email?: string | null;
+  address?: Address | null;
+  phone?: string | null;
+  website?: string | null;
+  company?: Company | null;
 }
 
 export interface Address {
